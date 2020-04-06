13a147486275481bc083d169887df4f41aff1c4d
Desiree Solomon <d.d.solomon4@gmail.com>
2020-04-06 16:04:22 -0400

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,10 +15,7 @@ const About = () => (
 
         <p>I look at coding as a puzzle. I love the process of creating the logic behinds apps and also figuring out a solution to all the problems that arise. I have learned a great deal about researching solutions, and the importance of networking when looking for help.  Currently, looking to work for a company that encourages teamwork and growth. </p>
 
-        
-        <div className="w3-col m6 w3-center w3-padding-large">
-     
-        </div>
+
         <br/>
           <div className="">
               <p><em>My Skills include.. </em></p>

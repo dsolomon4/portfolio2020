f22123999a4bd86083bf44ae308795dd4a4432be
Desiree Solomon <d.d.solomon4@gmail.com>
2020-04-06 16:05:36 -0400

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,19 +6,16 @@ const About = () => (
     <div className="aboutMe" id="about">
         <div className="aboutContainer">
 
-        <h3 className="">ABOUT ME</h3>
+        <h3 >ABOUT ME</h3>
         
 
-        <p>Hey All! I’m Desiree, an aspiring Junior Web Developer with over 15 years of Hospitality and Customer Service experience. I have a Bachelor’s Degree from Monroe College in Hospitality Management, and a Certification from the University of Central Florida in Full Stack Web Development. </p>
+        <p>Hi! I’m Desiree, an aspiring Junior Web Developer with over 15 years of Hospitality and Customer Service experience. I have a Bachelor’s Degree from Monroe College in Hospitality Management, and a Certification from the University of Central Florida in Full Stack Web Development. </p>
         
-
-
         <p>I look at coding as a puzzle. I love the process of creating the logic behinds apps and also figuring out a solution to all the problems that arise. I have learned a great deal about researching solutions, and the importance of networking when looking for help.  Currently, looking to work for a company that encourages teamwork and growth. </p>
 
-
         <br/>
-          <div className="">
-              <p><em>My Skills include.. </em></p>
+          <div >
+            <p><em>My Skills include.. </em></p>
 
             <p>React, Node, Express, HTML, MySql, Javascript, CSS</p>
           </div>
